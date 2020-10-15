b924c54c3c5bccecb8aef062899facdc2944ab00
hugsil <hugo.silva@matera.com>
2020-10-15 09:08:22 -0300

    refactor: change import of styles

--- web/src/pages/Landing/Landing.tsx
+++ web/src/pages/Landing/Landing.tsx
@@ -2,30 +2,30 @@ import React from 'react';
 import { FiArrowRight } from 'react-icons/fi';
 
 import logoImg from '~/images/logo.svg';
-import { Container, Content, Location, EnterButton } from './Landing.styles';
+import * as S from './Landing.styles';
 
 const Landing = () => {
   return (
-    <Container>
-      <Content>
-        <img src={logoImg} alt="Happy"/>
+    <S.Container>
+      <S.Content>
+        <img src={logoImg} alt="Happy" />
 
         <main>
           <h1>Leve felicidade para o mundo</h1>
           <p>Visite orfanatos e mude o dia de muitas crianças.</p>
         </main>
 
-        <Location>
+        <S.Location>
           <strong>Goiânia</strong>
           <span>Goiás</span>
-        </Location>
+        </S.Location>
 
-        <EnterButton to="/app" className="enter-app">
+        <S.EnterButton to="/app">
           <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
-        </EnterButton>
-      </Content>
-    </Container>
+        </S.EnterButton>
+      </S.Content>
+    </S.Container>
   );
-}
+};
 
 export default Landing;
